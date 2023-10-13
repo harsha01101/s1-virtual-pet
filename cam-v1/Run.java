450e571e4bbd9a552ceaf019482cf49b29c8f914
Harsha Ramesh <harshar24@ausdk12.org>
2023-10-13 11:23:11 -0700

    ok

--- cam-v1/Run.java
+++ cam-v1/Run.java
@@ -5,23 +5,39 @@ public class Run {
 
     public Run(){
             VirtualPet p = new VirtualPet();
-            p.takeabeat(2000);
-            String ans = getAnswer("How are you?");
+            p.takeabeat(3000);
+            int s = options("Are you perhaps....the marshland guide?", "Yes", "...No", "What is that??");
+            if (s == 0){
+                p.respondPositively();
+                p.takeabeat(2000);
+                p.setMessage("I need help travelling across the marshland."); 
+            } else if (s == 1){
+                p.setFace("normal");
+                p.setMessage("I see...");
+                p.takeabeat(2000);
+                int v = options("You can still help me though, right?", "Yes", "Yes", "Yes");
+                if (v == 0 || v == 1 || v == 2){
+                    p.respondPositively();
+                    p.takeabeat(2000);
+                    p.setMessage("I need help travelling across the marshland.");
+                }
+            } else {
+                p.setFace("annoyed");
+                p.takeabeat(2000);
+                p.setMessage("A guide. For the marshlands. You know, the place we're in right now?");
+                p.takeabeat(3000);
+                p.setMessage("It doesn't matter. I need help travelling across the marshland.");
+            }
+
+            p.takeabeat(3000);
+            p.setFace("marshland");
+            p.setMessage("These are the marshlands");
+            
+            p.takeabeat(3000);
+            String ans = getAnswer("I");
             if(ans.equalsIgnoreCase("Good")){
                 p.respondPositively();
             }
-            p.takeabeat(2000);
-            int s = options("Should I go to sleep?");
-            if (s == 0){
-                p.agree();
-                p.sleep();
-            } else if (s == 1){
-                p.agree();
-            } else {
-                p.setMessage("Okay. Goodnight.");
-                p.sleep();
-            }
-            
             /*p.exercise();
             p.exercise();
             p.exercise();
@@ -32,7 +48,6 @@ public class Run {
             
 
             }
-
         
 
     public static void main(String[] args){
@@ -50,11 +65,11 @@ public class Run {
         return s;
     }
     
-    public int options(String o){
-        Object[] options = {"Yes",
-                    "No",
-                    "Whatever you feel like"};
-        int n = JOptionPane.showOptionDialog( new JFrame(), o, "A Silly Question",
+    public int options(String o, String a, String b, String c){
+        Object[] options = {a,
+                    b,
+                    c};
+        int n = JOptionPane.showOptionDialog( new JFrame(), o, "Who are you?",
             JOptionPane.YES_NO_CANCEL_OPTION,
             JOptionPane.QUESTION_MESSAGE,
             null,
